172ba77f0edb4fb990eb24d24fcb5f5aeddfb0fc
CSxKING <csxkingjp@gmail.com>
2018-02-11 20:46:05 -0700

    test theme activation

--- rtm.js
+++ rtm.js
@@ -1,33 +1,37 @@
 var rtm = {
-	getThemeList: function(url) {
-		$.getJSON("")
-	},
 	loadTheme: function(url, name) {
-		$('head').append('<link id="rtm-' + name + '-theme" rel="stylesheet" href="' + url + '" />');
-	},
-	loadThemeList: function(localstorage) {
-
+		$("<link/>", {
+   			rel: "stylesheet",
+   			type: "text/css",
+   			id: "rtm-theme-" + name,
+   			href: url
+		}).appendTo("head");
 	},
 	loadThemeUrl: function(url, shouldsave) {
 
 	},
-	addMenuItem: function(onclick, id, text) {
-        $('#rtm-sect-t').append('<div class="rsrow" onclick="' + onclick + '"><div class="rscol-md-12 truncate"><div id="' + id + '" class="rcs item"><i class="icon icon-check-blue"></i><span>' + text + '</span></div></div></div>');
+	addMenuItem: function(theme, id, text) {
+        $('#rtm-sect-t').append('<a id="' + id + '" href="javascript:rtm.loadTheme(' + "'" + theme + "'" + ')"><div class="rsrow""><div class="rscol-md-12 truncate"><div class="rcs item"><i class="icon icon-check-blue"></i><span>' + text + '</span></div></div></div></a>');
     },
+    getThemeList: function(url) {
+		$.getJSON(url, function(data) {
+			$.each(data, function(key, value) {
+				rtm.addMenuItem(value, key, key);
+				console.warn(':::: ' + value + "::::");
+			});
+		});
+	},
 	on: function() {
 		// Create the menu section
 		$('#rcs-menu-t').after('<div class="group" id="rtm-sect"><span>Radiant Theme Manager</span></div>');
 		$('#rtm-sect').after('<div id="rtm-sect-t"></div>');
 		// Everything we append should go here
 
-		rtm.addMenuItem('alert("test");', 'testMenuItem', 'TestMenuItem');
+		rtm.getThemeList('https://rawgit.com/bentenz5/Radiant-Theme-Manager/master/themes.json');
 
 		// Credits and version
 	}
 };
 
-setTimeout(function() {
-	rtm.on();
-}, 500);
 
 //https://csxking.me/nce/THEME.css
