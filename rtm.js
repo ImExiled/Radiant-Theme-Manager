1e96f935ce41f943f2d08a92b2941dc24e578764
CSxKING <csxkingjp@gmail.com>
2018-02-18 06:10:09 -0700

    Theme loading
    Now loads saved theme settings. This means if the user saves a theme set on a community, it will load that theme set, including extra json files, and automatically enable the names of the saved. Theme saving has yet to be implemented, but its getting there.

--- rtm.js
+++ rtm.js
@@ -1,3 +1,36 @@
+// Functions for cookies :3
+// Note, I found these functions over on stackoverflow because i'm lazy ;p https://stackoverflow.com/questions/1458724/how-do-i-set-unset-a-cookie-with-jquery
+function readCookie(name) {
+    var nameEQ = encodeURIComponent(name) + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ')
+            c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0)
+            return decodeURIComponent(c.substring(nameEQ.length, c.length));
+    }
+    return null;
+}
+
+function createCookie(name, value, days) {
+    var expires;
+
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    } else {
+        expires = "";
+    }
+    document.cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + expires + "; path=/";
+}
+
+function eraseCookie(name) {
+    createCookie(name, "", -1);
+}
+
+
 var rtm = {
 	loadTheme: function(url, name) {
 		if( $('#' + name + '-theme-btn > .rsrow > .truncate > .item').hasClass('selected') )  {
@@ -41,7 +74,7 @@ var rtm = {
 		$(item + ' > .rsrow > .truncate > .item').removeClass('selected');
 	},
 	on: function(themelistfile) {
-		// Append jQuert 3.3.1 because plug hates me.
+		// Append jQuery 3.3.1 because plug hates me.
 		$('head').append('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>');
 
 		// Create the menu section
@@ -61,12 +94,55 @@ var rtm = {
 
 		// Credits and version
 	},
+	loadSavedThemes: function() {
+		// Load cookied themes
+
+		var jsonToLoad = readCookie('rtmSaved');
+		//var jsonToLoad = '{"lists":["https://rawgit.com/bentenz5/Radiant-Theme-Manager/master/themes.json"],"names":["NCE"]}';
+		console.warn('Json Txt: ' + jsonToLoad);
+		var parsedJson = JSON.parse(jsonToLoad);
+		$.each(parsedJson.lists, function(data) {
+			// Json files to render and load
+			console.warn('Parsed: ' + parsedJson.lists[data]);
+			rtm.getThemeList(parsedJson.lists[data]);
+		});
+		$.each(parsedJson.names, function(data) {
+			setTimeout(function() {
+			// Theme names
+			console.warn('Parsed Names: ' + parsedJson.names[data]);
+			var url = $('#' + parsedJson.names[data] + "-theme-btn").attr('href');
+			window.location = url;
+			}, 500);
+		});
+
+	},
+	onSaved: function() {
+		// If loading saved...
+		
+		// Append jQuery 3.3.1 because plug hates me.
+		$('head').append('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>');
+
+		// Create the menu section
+		$('#rs-external-site').before('<div class="group" id="rtm-sect"><span>Radiant Theme Manager</span></div>');
+		$('#rtm-sect').after('<div id="rtm-sect-t"></div>');
+
+		// Everything we append should go here
+		rtm.loadSavedThemes();
+
+		$('#chat-messages').append('<div class="cm rsshit sml message rs-log-green" id="rcs-1518950671516"><div class="badge-box"></div><div class="msg"><div class="from"><span class="timestamp" style="display: inline;">NA:NA</span></div><div class="text">RTM Loaded!</div></div><div class="rcs-delete" style="display: none;">Hide</div></div>');
+
+		// Credits and version
+	},
 	init: function() {
 		// This function serves to check for when RCS has fully loaded. Once it has, RTM will load.
 		if( $('#rcs-appright-button').length ) {
 			// rcs.loading == false
-			console.log('true');
-			rtm.on();
+
+			if( readCookie('rtmSaved') == null ) {
+				rtm.on();
+			} else {
+				rtm.onSaved();
+			}
 		} else {
 			console.error('RTM failed to load because RCS is not running. Retrying in 10 seconds...');
 			$('#chat-messages').append('<div class="cm rsshit sml message rs-log-red" id="rcs-1518950671516"><div class="badge-box"></div><div class="msg"><div class="from"><span class="timestamp" style="display: inline;">NA:NA</span></div><div class="text">RTM failed to load! Retrying in 10 seconds...</div></div><div class="rcs-delete" style="display: none;">Hide</div></div>');
