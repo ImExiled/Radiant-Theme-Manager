a8a2ab63611b45b852dee1ca73b3bbf102682b43
CSxKING <csxkingjp@gmail.com>
2018-02-18 04:27:48 -0700

    Themeing works correctly now
    You can now successfully toggle and untoggle themes by clicking them in the RTM menu in RCS. You can also chain-load themes, selecting multiple at once, sort of like a module system!

--- rtm.js
+++ rtm.js
@@ -1,16 +1,19 @@
 var rtm = {
 	loadTheme: function(url, name) {
-		$("<link/>", {
-   			rel: "stylesheet",
-   			type: "text/css",
-   			id: "rtm-theme-" + name,
-   			class: "rtm-theme",
-   			href: url
-		}).appendTo("head");
-		$('#' + name + '-theme-btn > .rcs.item').attr('disabled');
-	},
-	loadThemeUrl: function(url, shouldsave) {
+		if( $('#' + name + '-theme-btn > .rsrow > .truncate > .item').hasClass('selected') )  {
+			$('#rtm-theme-' + name).remove();
+			rtm.removeCheck('#' + name + '-theme-btn');
 
+		} else {
+			$("<link/>", {
+   				rel: "stylesheet",
+   				type: "text/css",
+   				id: "rtm-theme-" + name,
+   				class: "rtm-theme",
+   				href: url
+			}).appendTo("head");
+			rtm.addCheck('#' + name + '-theme-btn');
+		}
 	},
 	addThemeItem: function(theme, id, text) {
         $('#rtm-sect-t').append('<a id="' + id + '" href="javascript:rtm.loadTheme(' + "'" + theme + "', '" + text + "'" + ')"><div class="rsrow""><div class="rscol-md-12 truncate"><div class="rcs item"><i class="icon icon-check-blue"></i><span>' + text + '</span></div></div></div></a>');
@@ -31,6 +34,12 @@ var rtm = {
 			$('.rtm-theme').remove();
 		}, 500)
 	},
+	addCheck: function(item) {
+		$(item + ' > .rsrow > .truncate > .item').addClass('selected');
+	},
+	removeCheck: function(item) {
+		$(item + ' > .rsrow > .truncate > .item').removeClass('selected');
+	},
 	on: function() {
 		// Append jQuert 3.3.1 because plug hates me.
 		$('head').append('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>');
@@ -40,11 +49,27 @@ var rtm = {
 		$('#rtm-sect').after('<div id="rtm-sect-t"></div>');
 
 		// Everything we append should go here
-		rtm.addMenuItem('javascript:rtm.disableThemes();', 'Disable Themes', 'rtm-disable-btn');
 		rtm.getThemeList('https://rawgit.com/bentenz5/Radiant-Theme-Manager/master/themes.json');
 
+		$('#chat-messages').append('<div class="cm rsshit sml message rs-log-green" id="rcs-1518950671516"><div class="badge-box"></div><div class="msg"><div class="from"><span class="timestamp" style="display: inline;">NA:NA</span></div><div class="text">RTM Loaded!</div></div><div class="rcs-delete" style="display: none;">Hide</div></div>');
+
 		// Credits and version
+	},
+	init: function() {
+		// This function serves to check for when RCS has fully loaded. Once it has, RTM will load.
+		if( $('#rcs-appright-button').length ) {
+			// rcs.loading == false
+			console.log('true');
+			rtm.on();
+		} else {
+			console.error('RTM failed to load because RCS is not running. Retrying in 10 seconds...');
+			$('#chat-messages').append('<div class="cm rsshit sml message rs-log-red" id="rcs-1518950671516"><div class="badge-box"></div><div class="msg"><div class="from"><span class="timestamp" style="display: inline;">NA:NA</span></div><div class="text">RTM failed to load! Retrying in 10 seconds...</div></div><div class="rcs-delete" style="display: none;">Hide</div></div>');
+			setTimeout(function() {
+				rtm.init();
+			}, 10000);
+		}
 	}
 };
 
-rtm.on();
+
+rtm.init();
